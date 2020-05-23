1b484a66d9465100029547e4ccb16b20a0f61598
Shubham Sharma <shubham.sharma735@gmail.com>
2020-05-23 09:36:00 +0530

    Initial commit

--- wifiPasswordRetriver.py
+++ wifiPasswordRetriver.py
@@ -1,5 +1,4 @@
 import subprocess
-
 a=subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
 a=[i.split(':')[1][1:-1] for i in a if 'All User Profile' in i]
 for i in a:
